40c8d2ed4032739c2a25d724e1984925d2d13915
Jervis Muindi <jervisfm [at] gmail dot com>
2012-12-07 05:31:45 -0500

    remove unused function

--- userspace/src/gpsd/gpsd.c
+++ userspace/src/gpsd/gpsd.c
@@ -36,21 +36,6 @@
 
 static int should_exit = 0;
 
-
-static void print_gps(struct gps_location gps_location)
-{
-	unsigned long long int lat = 0, lng = 0;
-	unsigned int acc = 0;
-	lat = *((unsigned long long int*) &gps_location.latitude);
-	lng = *((unsigned long long int*) &gps_location.longitude);
-	acc = *((unsigned int*) &gps_location.accuracy);
-
-	/* still doesn't appear to work */
-	printf("Latitude: %#llx\n Longitude: %#llx\n Accuracy: %#x",
-			lat, lng, acc);
-}
-
-
 /*
  * My own get line function. I am writing my own because the
  * GNU getline fucntion is not defined under the ARM
