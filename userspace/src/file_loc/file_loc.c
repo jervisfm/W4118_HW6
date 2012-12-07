436b36d09192ad8c575a1e25398bd57cf2da43f9
Jervis Muindi <jervisfm [at] gmail dot com>
2012-12-07 07:36:43 -0500

    update the file_loc tool i forgot to do

--- userspace/src/file_loc/file_loc.c
+++ userspace/src/file_loc/file_loc.c
@@ -16,6 +16,28 @@
 #include <unistd.h>
 
 #include "file_loc.h"
+#define GMAP_URL "http://maps.googleapis.com/maps/api/staticmap?zoom=13" \
+"&size=800x800&maptype=roadmap&markers=color:blue|label:P|%f,%f&sensor=false"
+
+#define GET_GPS 377
+
+struct gps_location {
+	double latitude;
+	double longitude;
+	float  accuracy;  /* in meters */
+};
+
+static void print_gps(struct gps_location gps_location)
+{
+	double lat = 0, lng = 0;
+	float acc = 0;
+	lat = gps_location.latitude;
+	lng = gps_location.longitude;
+	acc = gps_location.accuracy;
+
+	printf("Latitude: %f\n Longitude: %f\n Accuracy: %fm\n",
+			lat, lng, acc);
+}
 
 static void sighandler(int sig);
 
@@ -23,7 +45,28 @@ static void sighandler(int sig);
 static int do_file_loc(const char *path)
 {
 	/* print Google Maps URL based on path, return -1 on failure */
+	long int ret;
+	struct gps_location loc;
 
+	if (path == NULL)
+		return -1;
+
+	printf("GPS File Reader Tool\n");
+	memset(&loc, 0, sizeof(loc));
+
+	/* Retrieve File GPS Info From Kernel */
+	ret = syscall(GET_GPS, path, &loc);
+	if (ret < 0 ) {
+		perror("GPS Retrieval System call failed:");
+		return -1;
+	} else {
+		printf("Retrieved GPS Information:\n");
+		print_gps(loc);
+		printf("\nAge of GPS info is %ld seconds\n", ret);
+		printf("Google Maps URL: ");
+		printf(GMAP_URL, loc.latitude, loc.longitude);
+		printf("\n");
+	}
 	return 0;
 }
 
