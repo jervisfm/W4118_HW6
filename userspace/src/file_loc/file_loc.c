30a8fd15045aed3429e3065b0873d53a2e6f25a6
Jervis Muindi <jervisfm [at] gmail dot com>
2012-12-08 00:07:33 -0500

    remove directory check in file_loc

--- userspace/src/file_loc/file_loc.c
+++ userspace/src/file_loc/file_loc.c
@@ -75,17 +75,6 @@ static void usage(char **argv)
 	printf("Usage: %s <file>\n", argv[0]);
 	exit(0);
 }
-/* Tries to determine if the given path is a directory or not.
- * Definitely Always Returns 1 on true and 0 if false with high probability. */
-static int is_directory(char *path)
-{
-
-	DIR* result = opendir(path);
-	if (result == NULL) /* Failed to open DIR, likely path not dir */
-		return 0;
-	else  /* Definitely is a directory */
-		return 1;
-}
 
 int main(int argc, char **argv)
 {
@@ -96,12 +85,6 @@ int main(int argc, char **argv)
 	if (argc != 2)
 		usage(argv);
 
-	if (is_directory(argv[1])) {
-		printf("Directory Path Entered.\n"
-		      "GPS information is only available for files and not"
-		      "Directories\n");
-		return EXIT_FAILURE;
-	}
 
 	if (do_file_loc(argv[1]) < 0) {
 		printf("No GPS information: \n");
